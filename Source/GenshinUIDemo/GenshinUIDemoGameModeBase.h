850284879d5b86836129ceedad4325e73adba47a
Zydiii <zydiiii@outlook.com>
2022-10-05 09:37:29 +0800

    first commit

---
+++ Source/GenshinUIDemo/GenshinUIDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GenshinUIDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHINUIDEMO_API AGenshinUIDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
